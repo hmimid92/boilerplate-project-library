a2b0d40de226b4a938b6267c235200af2aea9fbb
HMIMID <ahmedhmimid177@gmail.com>
2024-12-04 20:28:00 +0100

    update functional-test.js file

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -187,7 +187,10 @@ suite('Functional Tests', function() {
         chai
         .request(server)
         .keepOpen()
-        .delete('/api/books/6750a8c99f597337463a251d')
+        .delete('/api/books/:id')
+        .send({
+          _id: "6750a8c99f597337463a251d"
+        })
         .end(function(err, res){
           assert.equal(res.status, 200);
           assert.isString(res.text, 'response should be a string');
