258fff590ad9096017a40c0ab3751a858f27e55d
HMIMID <ahmedhmimid177@gmail.com>
2024-12-04 21:00:11 +0100

    update functional-test.js file

--- routes/api.js
+++ routes/api.js
@@ -113,11 +113,8 @@ module.exports = function (app) {
             comments: [...bookFound.comments, comment],
             commentcount: bookFound.commentcount + 1
           }, {new: true});
-          if(!bookUpdated) {
-            res.send("could not update");
-          } else {
             res.json(bookUpdated);
-          }
+            return;
         }
     })
     
